d28cf10f6419164c155664de79d15dfb839bf39e
Asif2535 <aiqbal517@gmail.com>
2021-03-31 01:02:53 +0600

    create a home page navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand className="ml-5">Foodeli</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto ml-5">
+                        <Nav.Link className="ml-5">Home</Nav.Link>
+                        <Nav.Link className="ml-5">Orders</Nav.Link>
+                        <Nav.Link className="ml-5">Admin</Nav.Link>
+                        <Nav.Link className="ml-5">Deals</Nav.Link>
+                        <Button variant="outline-success" className="ml-5">Login</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
